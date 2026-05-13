b12179b99eaadf775a926528d8b802db3ebadbe9
geniousabby <abbyspam1228@gmail.com>
2026-05-13 13:48:09 -0700

    search and .findall
    made a regex pattern for phone numbers and email addresses and also used .findall for email and phone numbers

--- main.py
+++ main.py
@@ -63,7 +63,11 @@ import pyperclip
 text = pyperclip.paste()
 
 # Search for phone numbers + emails
+phone_regex = re.compile(r'\d{3}-\d{3}-\d{4}') # This compiles a regex pattern for phone numbers in the format 123-456-7890
+email_regex = re.compile(r'\w+@\w+\.\w+') # This compiles a regex pattern for email addresses in the format user@domain.extension
 
+phone_numbers = phone_regex.findall(text)
+emails = email_regex.findall(text)
 
 # Replace text in keyboard with found phone numbers and email
 # pyperclip.copy() will put the new text back onto the clipboard
