15b295c3cf978afe3c3d87cb84dc5550735059c9
geniousabby <abbyspam1228@gmail.com>
2026-05-13 14:37:52 -0700

    explained
    added pseudo code to explain the .findall code

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ text = pyperclip.paste()
 phone_regex = re.compile(r'\d{3}-\d{3}-\d{4}') # This compiles a regex pattern for phone numbers in the format 123-456-7890
 email_regex = re.compile(r'\w+@\w+\.\w+') # This compiles a regex pattern for email addresses in the format user@domain.extension
 
-phone_numbers = phone_regex.findall(text)
-emails = email_regex.findall(text)
+phone_numbers = phone_regex.findall(text) # This finds anything with the phone number pattern in the text and returns them as a list
+emails = email_regex.findall(text) # This finds anything with the email pattern in the text and returns them as a list
 
 # Replace text in keyboard with found phone numbers and email
 # pyperclip.copy() will put the new text back onto the clipboard
